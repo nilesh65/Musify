c7987ba221727b331b69b1a895ee43b63bbdb279
niles <nileshmkjee123@gmail.com>
2025-12-31 22:02:26 +0530

    Frontend completed

---
+++ src/middleware.ts
@@ -0,0 +1,57 @@
+import type { NextFunction, Request, Response } from "express";
+import { User, type IUser } from "./model.js";
+import  jwt, { type JwtPayload } from "jsonwebtoken";
+
+
+export interface AuthenticatedRequest extends Request {
+  user?: IUser | null;
+}
+
+export const isAuth = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const token = req.headers.token as string;
+
+    if (!token) {
+      res.status(403).json({
+        message: "Please Login",
+      });
+
+      return;
+    }
+
+    const decodedValue = jwt.verify(
+      token,
+      process.env.JWT_SEC as string
+    ) as JwtPayload;
+
+    if (!decodedValue || !decodedValue._id) {
+      res.status(403).json({
+        message: "Invalid token",
+      });
+      return;
+    }
+
+    const userId = decodedValue._id;
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      res.status(403).json({
+        message: "User Not found",
+      });
+
+      return;
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(403).json({
+      message: "Please Login",
+    });
+  }
+};
